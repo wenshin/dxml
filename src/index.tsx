f657c817ccfd550057fbb6c9e73a69048047e95d
yiheng <wenshin2011@gmail.com>
2022-01-20 21:27:15 +0800

    chore: 预编译

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import { render } from 'react-snapshot';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './dxml';
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+if (rootElement && rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
